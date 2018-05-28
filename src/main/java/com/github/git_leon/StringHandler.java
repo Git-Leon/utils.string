d38367874064ffee9bb455ede9b163a424235728
Leon <leon@Leons-MacBook-Pro.local>
2018-05-28 15:19:38 -0400

    created `StringHandler` with a `getSubStringBetweenSubStrings` method; more to be added later I suppose

---
+++ src/main/java/com/github/git_leon/StringHandler.java
@@ -0,0 +1,45 @@
+package com.github.git_leon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author leon on 5/28/18.
+ */
+public class StringHandler {
+    private final String stringToBeHandled;
+
+    /**
+     * @param stringToBeHandled the value of some string to be evaluated
+     */
+    public StringHandler(String stringToBeHandled) {
+        this.stringToBeHandled = stringToBeHandled;
+    }
+
+    /**
+     * @param subString1 first sub string to target
+     * @param subString2 second sub string to target
+     * @return the first substring to be found between a specified `subString1` and `subString2`
+     */
+    public String getSubStringsBetweenSubStrings(String subString1, String subString2) {
+        return getSubStringBetweenSubStrings(subString1, subString2).get(0);
+    }
+
+
+    /**
+     * @param subString1 first sub string to target
+     * @param subString2 second sub string to target
+     * @return a list of each substring to be found between a specified `subString1` and `subString2`
+     */
+    public List<String> getSubStringBetweenSubStrings(String subString1, String subString2) {
+        Pattern pattern = Pattern.compile(subString1 + "(.*?)" + subString2);
+        Matcher matcher = pattern.matcher(stringToBeHandled);
+        List<String> stringList = new ArrayList<String>();
+        while (matcher.find()) {
+            stringList.add(matcher.group(1));
+        }
+        return stringList;
+    }
+}
---
+++ src/test/java/com/github/git_leon/stringhandler/GetSubStringBetweenSubStringsTest.java
@@ -0,0 +1,39 @@
+package com.github.git_leon.stringhandler;
+
+import com.github.git_leon.StringHandler;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author leon on 5/28/18.
+ */
+public class GetSubStringBetweenSubStringsTest {
+
+    @Test
+    public void testBetweenJOJOandBOBO() {
+        test("JOJO", "BOBO", "The quick brown fox jumps over the lazy dog");
+    }
+
+    @Test
+    public void testBetweenHtmlCommentTags() {
+        test("<!--", "-->", "The quick brown fox jumps over the lazy dog");
+    }
+
+
+    @Test
+    public void testBetweenGreaterThanPercentEqualsAndEqualsPercentLessThan() {
+        test("<%=", "=%>", "The quick brown fox jumps over the lazy dog");
+    }
+
+    public void test(String prefix, String suffix, String body) {
+        // Given
+        String stringToBeHandled = prefix + body + suffix;
+        StringHandler stringHandler = new StringHandler(stringToBeHandled);
+
+        // When
+        String actual = stringHandler.getSubStringsBetweenSubStrings(prefix, suffix);
+
+        // Then
+        Assert.assertEquals(body, actual);
+    }
+}
