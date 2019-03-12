60cf4a0e5a3bc4cc1eec51ca4f29a23f5d047967
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-03-12 18:42:45 -0400

    updated README versioning

---
+++ src/main/java/com/github/git_leon/regex/MatchGroup.java
@@ -0,0 +1,45 @@
+package com.github.git_leon.regex;
+
+import com.github.git_leon.collectionutils.ListFacade;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+
+public class MatchGroup extends ListFacade<Match> {
+    private final Matcher matcher;
+
+    public MatchGroup(List<Match> matchList, Matcher matcher) {
+        super(matchList);
+        this.matcher = matcher;
+        this.initializeMatchers();
+    }
+
+    public MatchGroup(Matcher matcher) {
+        this(new ArrayList<>(), matcher);
+    }
+
+    public Matcher getMatcher() {
+        return matcher;
+    }
+
+    public List<Match> toList() {
+        return super.toList();
+    }
+
+    @Override
+    public String toString() {
+        return toList().toString();
+    }
+
+    private void initializeMatchers() {
+        for (int i = 0; matcher.find(); i++) {
+            super.add(new MatchBuilder()
+                    .setStartingIndex(matcher.start())
+                    .setEndingIndex(matcher.end())
+                    .setValue(matcher.group())
+                    .setMatchNumber(i)
+                    .build());
+        }
+    }
+}
